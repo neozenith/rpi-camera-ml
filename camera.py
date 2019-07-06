e809687aad6fc71e61d026bd220ddebcf5bc46a1
Josh Peak <neozenith.dev@gmail.com>
2019-07-06 22:39:10 +1000

    Record short video

--- camera.py
+++ camera.py
@@ -3,6 +3,10 @@ from time import sleep
 camera = PiCamera()
 
 camera.rotation = 180
+camera.resolution = (1024, 768)
+camera.framerate = 15
 camera.start_preview(alpha=200)
+camera.start_recording('/home/pi/Desktop/video.h264')
 sleep(10)
+camera.stop_recording()
 camera.stop_preview()
