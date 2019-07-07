48982d7b649d34b51c82d67a2ff1591ec1679c29
Josh Peak <neozenith.dev@gmail.com>
2019-07-07 11:10:29 +1000

    refactor camera settings and use context manager

--- camera.py
+++ camera.py
@@ -1,12 +1,13 @@
 from picamera import PiCamera
 from time import sleep
-camera = PiCamera()
 
-camera.rotation = 180
-camera.resolution = (1024, 768)
-camera.framerate = 15
-camera.start_preview(alpha=200)
-camera.start_recording('/home/pi/Desktop/video.h264')
-sleep(10)
-camera.stop_recording()
-camera.stop_preview()
+cam_settings = dict(resolution=(1024, 768), framerate=15)
+prev_settings = dict(alpha=200)
+
+with PiCamera(**cam_settings) as camera:
+    camera.rotation = 180
+    camera.start_preview(**prev_settings)
+    camera.start_recording("/home/pi/Desktop/video.h264")
+    sleep(10)
+    camera.stop_recording()
+    camera.stop_preview()
